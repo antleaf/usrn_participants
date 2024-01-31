95949ea42eadf1de65c174a9eb674261b9ec483c
paulwalk <paul@paulwalk.net>
2024-01-31 09:12:51 +0000

    added datatables

--- lib/web_sinatra_helpers.rb
+++ lib/web_sinatra_helpers.rb
@@ -4,4 +4,20 @@ helpers do
   def process_text(text)
     text.gsub(/(\r\n|\n|\r)/, '<br />')
   end
+
+  def humanise(text)
+    text.to_s.gsub(/_/,' ').capitalize
+  end
+
+  def missions(sr)
+    [
+      {value: sr.mission_keeping_track, name: 'Keep track of outputs'},
+      {value: sr.mission_increase_discoverability, name: 'Increase discoverability'},
+      {value: sr.mission_capture_metadata, name: 'Capture metadata'},
+      {value: sr.mission_avoid_duplication, name: 'Avoid duplication'},
+      {value: sr.mission_populate_pids, name: 'Populate PIDs'},
+      {value: sr.mission_timely_oa, name: 'Timely OA'},
+      {value: sr.mission_machine_accessible, name: 'Machine accessible'}
+    ]
+  end
 end
