cfcd732123c21d3f1e483cf0951852d5a0473150
paulwalk <paul@paulwalk.net>
2024-02-19 13:11:31 +0000

    reconfigured as modular app using Rack

--- cmd.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-mkdir -p /usr/src/app/src
-# shellcheck disable=SC2164
-cd /usr/src/app/src
-git clone -q https://github.com/antleaf/usrn_participants.git .
-gem install bundler
-bundle install
-bundle exec rackup
