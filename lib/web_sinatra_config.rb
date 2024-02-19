8a48b576f927fae419f3083eea7775a03b4500c4
paulwalk <paul@paulwalk.net>
2024-02-19 11:24:10 +0000

    reconfigured as modular app using Rack

--- lib/web_sinatra_config.rb
+++ lib/web_sinatra_config.rb
@@ -1,10 +1,15 @@
 require 'sinatra'
 require 'haml'
 
-set :haml, { escape_html: false }
-set :views, settings.root + '/web/templates'
-set :public_folder, settings.root + '/web/assets'
-set :site_title, 'USRN Participants'
-set :default_survey_id, 'survey-1'
-
-enable :sessions
+class USRNParticipantsApp < Sinatra::Base
+  set :haml, { escape_html: false }
+  set :views, settings.root + '/web/templates'
+  set :public_folder, settings.root + '/web/assets'
+  set :site_title, 'USRN Participants'
+  set :default_survey_id, 'survey-1'
+  enable :logging
+  enable :sessions
+  # configure :production, :development do
+  #
+  # end
+end
