0d197ce872c9b041b5c9cca59aaf844b512d5548
paulwalk <paul@paulwalk.net>
2024-02-22 15:32:45 +0000

    added owner columns to tables

--- lib/routes/disciplines.rb
+++ lib/routes/disciplines.rb
@@ -1,6 +1,6 @@
 class USRNParticipantsApp < Sinatra::Base
   get '/disciplines/?' do
-    @page_title = "disciplines"
+    @page_title = "Disciplines"
     @sr = SurveyResponse.where(survey_id: session[:survey_id])
     haml :disciplines, :layout => :'layout'
   end
